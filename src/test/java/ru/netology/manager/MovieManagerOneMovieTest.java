c9018716eaff8f04e1520771178e6224f2140349
Rusdos <mr.Nurgaleev@gmail.com>
2020-11-26 15:37:12 +0500

    Editing shouldGetLast

--- src/test/java/ru/netology/manager/MovieManagerOneMovieTest.java
+++ src/test/java/ru/netology/manager/MovieManagerOneMovieTest.java
@@ -7,16 +7,18 @@ import ru.netology.domain.Movie;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MovieManagerOneMovieTest {
-    private MovieManager manager = new MovieManager();
+   // private MovieManager manager = new MovieManager();
     private Movie first = new Movie(1, "Бладшот", "Боевик", "", false);
 
-    @BeforeEach
+ /*   @BeforeEach
     public void setUp() {
         manager.add(first);
-    }
+    }*/
 
     @Test
     public void shouldRemoveOneMovie() {
+        MovieManager manager = new MovieManager();
+        manager.add(first);
         int idToRemove = 1;
         manager.removeById(idToRemove);
         Movie[] actual = manager.getLast();
@@ -25,8 +27,10 @@ public class MovieManagerOneMovieTest {
     }
 
     @Test
-    public void shouldGetAll(){
-        Movie[] actual = manager.getLast();
+    public void shouldGetLast(){
+        MovieManager managerGet = new MovieManager();
+        managerGet.add(first);
+        Movie[] actual = managerGet.getLast();
         Movie[] expected = new Movie[]{first};
         assertArrayEquals(expected, actual);
     }
